a2b0ae84e0db09c7f7566a8b944f4172526052a2
Alina Momin <alinam1@umbc.edu>
2021-12-16 13:29:28 -0600

    updated test1.py to automate drone

--- test1.py
+++ test1.py
@@ -5,17 +5,24 @@ import time
 tello = Tello()
 
 tello.connect()
-
-
+tello.wait_for_connection(10.0)
 tello.takeoff()
 
-#tello.move_left(100)
-tello.rotate_clockwise(90)
-#tello.move_forward(100)
-print(tello.get_current_state())
 
-tello.rotate_counter_clockwise(90)
+tello.move_left(100)
+time.sleep(5)
+#tello.rotate_clockwise(90)
+tello.move_forward(100)
+time.sleep(5)
+tello.move_right(100)
+time.sleep(5)
+tello.move_back(100)
+time.sleep(5)
 
-time.sleep(3)
+
+tello.move_down(100)
+#print(tello.get_current_state())
+
+#tello.rotate_counter_clockwise(90)
 
 tello.land()
