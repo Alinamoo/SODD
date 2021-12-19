91c5bd82fdd2c5f17896ba1a4463895972781c56
Alinamoo <43764635+Alinamoo@users.noreply.github.com>
2021-12-19 15:42:44 -0600

    Update missionpad_test.py
    updated test for mission pad 1 detection

--- missionpad_test.py
+++ missionpad_test.py
@@ -7,19 +7,42 @@ tello = Tello()
 tello.connect()
 print(tello.get_current_state())
 
-tello.takeoff()
-
 tello.enable_mission_pads()
+tello.set_mission_pad_detection_direction(1)
+
+tello.takeoff()
+ pad = tello.get_mission_pad_id()
 
 
+#detect and react to pads until we see pad #1
+#pad 1 is top left
+#pad 2 is top right
+#pad 3 is bottom left
+#pad 4 is bottom right
 
-tello.go_xyz_speed_mid(25, 25, 120, 20, 1)
+while pad != 1:
+    if pad == 3:
+        tello.move_up(30)
 
-tello.go_xyz_speed_mid(-50, -50, 120, 20, 1)
+    if pad == 4:
+        tello.move_up(30)
+        tello.move_left(30)
+
+    if pad == 2:
+        tello.move_left(30)
+
+    pad = tello.get_mission_pad_id()
+
+
+#termination
+tello.disable_mission_pads()
+tello.land()
+tello.end()
 
 #tello.go_xyz_speed_mid(25, 25, 120, 20, 1)
 
-tello.go_xyz_speed_mid(0, 0, 50, 20, 1)
+#tello.go_xyz_speed_mid(-50, -50, 120, 20, 1)
 
+#tello.go_xyz_speed_mid(25, 25, 120, 20, 1)
 
-tello.land()
+#tello.go_xyz_speed_mid(0, 0, 50, 20, 1)
