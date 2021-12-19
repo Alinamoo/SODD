a59888223fbcb6c4eaf3e76a3b3b8c298c5d1d26
byamada1 <44327610+byamada1@users.noreply.github.com>
2021-12-19 17:02:30 -0500

    Updating test1 to loop

--- test1.py
+++ test1.py
@@ -5,22 +5,22 @@ import time
 tello = Tello()
 
 tello.connect()
-tello.wait_for_connection(10.0)
+#tello.wait_for_connection(10.0)
 tello.takeoff()
 
+for i in range(4):
+    tello.move_left(100)
+    time.sleep(1)
+    #tello.rotate_clockwise(90)
+    tello.move_forward(100)
+    time.sleep(1)
+    tello.move_right(100)
+    time.sleep(1)
+    tello.move_back(100)
+    time.sleep(1)
 
-tello.move_left(100)
-time.sleep(5)
-#tello.rotate_clockwise(90)
-tello.move_forward(100)
-time.sleep(5)
-tello.move_right(100)
-time.sleep(5)
-tello.move_back(100)
-time.sleep(5)
 
-
-tello.move_down(100)
+#tello.move_down(100)
 #print(tello.get_current_state())
 
 #tello.rotate_counter_clockwise(90)
